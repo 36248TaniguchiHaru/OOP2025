3516d5d5f290435b8dcb3994b673f85b54c6e318
TaniguchiHaru <oibc36248@ojs.ac.jp>
2025-05-09 15:00:29 +0900

    メソッドの移動

--- Chapter02/SalesCalculator/SalesCounter.cs
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -23,5 +23,20 @@ namespace SalesCalculator{
             }
             return dict;
         }
+        public static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale() {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2]),
+                };
+                sales.Add(sale);
+            }
+
+            return sales;
+        }
     }
 }
