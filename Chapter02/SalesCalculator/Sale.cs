fb8ecf48a82ea9b185ee9077edace4bc90fdacd2
TaniguchiHaru <oibc36248@ojs.ac.jp>
2025-05-09 13:55:45 +0900

    売り上げ集計プログラム初版完成

---
+++ Chapter02/SalesCalculator/Sale.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator{
+   public class Sale{
+        public string ShopName { get; set; } = String.Empty;
+        public string ProductCategory { get; set; } = String.Empty;
+        public int Amount { get; set; }
+    }
+}
---
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator{
+    public class SalesCounter {
+        private readonly List<Sale> _sales;
+
+        public SalesCounter(List<Sale> sales) {
+            _sales = sales;
+        }
+
+        public Dictionary<string,int> GetPerStoreSales() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach(Sale sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amount;
+                else
+                    dict[sale.ShopName] = sale.Amount;
+            }
+            return dict;
+        }
+    }
+}
