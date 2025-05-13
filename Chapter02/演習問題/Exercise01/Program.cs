f90199f114074c8f2c240412938505fc57d02286
TaniguchiHaru <oibc36248@ojs.ac.jp>
2025-05-13 10:22:21 +0900

    2.1.1完成

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise01 {
+    public class Program {
+        static void Main(string[] args) {
+            //2.1.3
+    
+
+
+        }
+
+        //2.1.4
+        private static void printSongs(Song[] songs) {
+
+        }
+
+    }
+}
---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //2.1.1
+
+    public class Song {
+        string Title;
+        string ArtistName;
+        int Length;
+
+        //2.1.2
+       
+       
+    }
+    
+
+}
