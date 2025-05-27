f261c612f7d38bdbd451cebe504dea62e5a86aef
TaniguchiHaru <oibc36248@ojs.ac.jp>
2025-05-27 11:00:04 +0900

    7.1.3

---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System.Text;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+
+            Console.WriteLine("7.1.1");
+            Exercise1(numbers);
+
+            Console.WriteLine("7.1.2");
+            Exercise2(numbers);
+
+            Console.WriteLine("7.1.3");
+            Exercise3(numbers);
+
+            Console.WriteLine("7.1.4");
+            Exercise4(numbers);
+
+            Console.WriteLine("7.1.5");
+            Exercise5(numbers);
+        }
+
+        private static void Exercise1(int[] numbers) {
+            Console.WriteLine(numbers.Max());
+        }
+
+        private static void Exercise2(int[] numbers) {
+            Console.WriteLine(string.Join("",numbers.Reverse().Take(2)));
+            foreach(var n in numbers.Skip(numbers.Length-2)) {
+                Console.WriteLine(n);
+            }
+        }
+
+        private static void Exercise3(int[] numbers) {
+            for (int i = 0; i < numbers.Length; i++) {
+                Console.WriteLine("{0,3}",numbers[i].ToString("D3"));
+            }
+        }        
+
+        private static void Exercise4(int[] numbers) {
+        }
+
+        private static void Exercise5(int[] numbers) {
+            
+        }
+    }
+}
---
+++ LINQ/LinqSample/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinqSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var numbers = Enumerable.Range(1, 100);
+
+            Console.WriteLine(numbers.Where(n=>n%8==0).Sum());
+
+            foreach(var num in numbers) {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
