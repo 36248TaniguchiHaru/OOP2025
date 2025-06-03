bb54e715d1a72fb05a6444a1409701af5f8d23dc
TaniguchiHaru <oibc36248@ojs.ac.jp>
2025-06-03 10:12:11 +0900

    演習問題6.6.4

---
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+
+            Exercise04(line);
+
+        }
+
+        private static void Exercise04(string line) {
+            var newline = line.Split('=',';');
+            for (int count=0; count < newline.Length;count++ ) {
+                if (newline[count] == "Novelist" && newline[count+1]is not null) {
+                    Console.WriteLine("作家:" + newline[count + 1]);
+                }else if (newline[count] == "BestWork" && newline[count + 1] is not null) {
+                    Console.WriteLine("代表作:" + newline[count + 1]);
+                } else(newline[count] == "Born" && newline[count + 1] is not null) {
+                    Console.WriteLine("誕生年:" + newline[count + 1]);
+                }
+            }
+               
+            
+            
+        }
+    }
+}
