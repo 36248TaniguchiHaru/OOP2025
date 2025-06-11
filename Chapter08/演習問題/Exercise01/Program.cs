0f23880777ce1543b071bedaaedcfa97c9b9fc37
Haru Taniguchi <oibc36248@ojs.ac.jp>
2025-06-11 11:28:54 +0900

    8.1.1

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+
+            Exercise2(text);
+
+        }
+
+        private static void Exercise1(string text) {
+            var countDict = new Dictionary<char, int>(); 
+            foreach (var ch in text.ToUpper()) {
+                if ('A' <= ch && ch <= 'Z') {
+                if (countDict.ContainsKey(ch)) {
+                    countDict[ch]++;     
+                } else {
+                    countDict[ch]= 1;
+                }
+            }
+                foreach (var (key,value) in countDict.OrderBy(s=>s.Key)) {
+                    Console.WriteLine($"{ key}:{value}");
+                }
+            
+        }
+            
+            
+            /*for(var ch='A'; 'A' <= ch && ch <= 'Z'; ch++) {
+                var count = text.ToUpper().Count(s=>s==ch);
+                Console.WriteLine("'" + ch + "':" + count);
+            }*/
+        }
+
+        private static void Exercise2(string text) {
+            
+        }
+    }
+}
