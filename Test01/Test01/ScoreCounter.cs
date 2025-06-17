109fea84fd44420d91d731b0e9f9e092ab5ab784
Haru Taniguchi <oibc36248@ojs.ac.jp>
2025-06-17 11:31:30 +0900

    設問1_Studentクラス完成

---
+++ Test01/Test01/ScoreCounter.cs
@@ -0,0 +1,47 @@
+﻿namespace Test01 {
+    public class ScoreCounter {
+        private IEnumerable<Student> _score;
+
+        // コンストラクタ
+        public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
+
+        }
+
+        //メソッドの概要： 
+        private static IEnumerable<Student> ReadScore(string filePath) {
+            var score = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach(var line in lines) {
+                var items = line.Split(',');
+                var scores = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                score.Add(scores);
+                
+                
+            }
+
+            return score;
+
+
+        }
+
+        //メソッドの概要： 
+        public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new Dictionary<string, int>();
+            foreach (var scores in _score) {
+                if (dict.ContainsKey(scores.Subject))
+                    dict[scores.Subject] += scores.Score;
+                else
+                    dict[scores.Subject] = scores.Score;
+            }
+            return dict;
+
+
+
+        }
+    }
+}
---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        public string Name { get; set; } = String.Empty;
+        public string Subject { get; set; } = String.Empty;
+        public int Score { get; set; }
+    }
+}
