b8cd21bdc444de1e6bd17ca8bcc4b311323626f8
Haru Taniguchi <oibc36248@ojs.ac.jp>
2025-08-22 12:10:25 +0900

    演習問題１

---
+++ Chapter11/演習問題/Exercise01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(IsPhoneNumber("080-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("060-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("190-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-9111-12341"));
+            Console.WriteLine(IsPhoneNumber("A090-911-1234"));
+            Console.WriteLine(IsPhoneNumber("090-1911-234"));
+        }
+
+        private static bool IsPhoneNumber(string telNum) {
+            return Regex.IsMatch(telNum, @"^090|^080|^070");
+        }
+    }
+}
