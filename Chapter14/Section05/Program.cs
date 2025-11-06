eeadde11795a111b7d3b02b341293a5678c7b12e
Haru Taniguchi <oibc36248@ojs.ac.jp>
2025-11-06 15:17:33 +0900

    並列処理ありとなしの比較サンプル

---
+++ Chapter14/Section05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+            Parallel.For(0, 100, i => {
+                Console.WriteLine($"処理{i}開始");
+                Thread.Sleep(100);
+                Console.WriteLine($"処理{i}終了");
+            });
+
+            Console.WriteLine("並列処理なし");
+
+            for(int i = 0; i < 100; i++) {
+                Console.WriteLine($"処理{i}開始");
+                Thread.Sleep(100);
+                Console.WriteLine($"処理{i}終了");
+            }
+        }
+    }
+}
