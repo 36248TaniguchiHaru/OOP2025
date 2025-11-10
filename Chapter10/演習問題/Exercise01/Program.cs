73b618dad5bb0062380f030998fd55bc8ec93ad4
Haru Taniguchi <oibc36248@ojs.ac.jp>
2025-11-10 14:26:38 +0900

    Chapter10問題10.1①

---
+++ Chapter10/演習問題/Exercise01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+using System.Text.RegularExpressions;
+
+namespace Exercise01 {
+    class Program {
+        static void Main() {
+            string filePath = @"C:\Users\infosys\source\repos\OOP2025\Chapter10\演習問題\Exercise01\Load.cs";
+
+            using (StreamReader reader = new StreamReader(filePath)) {
+                int count=0;
+                string line;
+                while ((line = reader.ReadLine()) != null) {
+                    if (Regex.IsMatch(line,@"\sclass\s")) {
+                        count++;  
+                    }
+                }
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
+
--- Chapter14/Section05/Program.cs
+++ Chapter14/Section05/Program.cs
@@ -1,21 +1,34 @@
-﻿using System.Security.Cryptography;
+﻿using Section01;
+using System.Diagnostics;
+using System.Security.Cryptography;
 
 namespace Section05 {
     internal class Program {
-        static void Main(string[] args) {
-            Parallel.For(0, 100, i => {
-                Console.WriteLine($"処理{i}開始");
-                Thread.Sleep(100);
-                Console.WriteLine($"処理{i}終了");
-            });
+        static async Task Main(string[] args) {
+            var ta = new TaskExample();
+            await ta.Run();
+        }  
+    }
 
-            Console.WriteLine("並列処理なし");
+    // Code 14.22
+    public class TaskExample {
+        private readonly HttpClient _httpClient = new HttpClient();
 
-            for(int i = 0; i < 100; i++) {
-                Console.WriteLine($"処理{i}開始");
-                Thread.Sleep(100);
-                Console.WriteLine($"処理{i}終了");
-            }
+        public async Task Run() {
+            var tasks = new Task<string>[] {
+            GetPageAsync(@"https://business.nikkei.com/rss/sns/nb.rdf"),
+            GetPageAsync(@"https://www.nhk.or.jp/rss/news/cat3.xml"),
+        };
+            var results = await Task.WhenAll(tasks);
+
+            // それぞれ先頭400文字を表示する
+            var text = $"{results[0].Substring(0, 400)}\n\n{results[1].Substring(0, 400)}";
+            Console.WriteLine(text);
+        }
+
+        private async Task<string> GetPageAsync(string urlstr) {
+            var str = await _httpClient.GetStringAsync(urlstr);
+            return str;
         }
     }
 }
