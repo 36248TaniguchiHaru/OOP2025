f6fee657958de735e2f7d13520aea4f3df6cef21
Haru Taniguchi <oibc36248@ojs.ac.jp>
2025-11-17 15:57:26 +0900

    15.1 LineToHalfNumberService 完成

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineToHalfNumberService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    public class LineToHalfNumberService : ITextFileService {
+        public void intialize(string fname) {
+            
+        }
+
+        public void Execute(string line) {
+            Console.WriteLine(ConvertToHalfWidth(line));
+
+            //　テスト用txtファイルパス
+            //    C:\Users\infosys\Documents\Halftest.txt
+        }
+
+        public void Terminate() {
+            
+        }
+
+        static string ConvertToHalfWidth(string input) {
+            return string.Join("", input.Select(c => char.GetNumericValue(c) >= 0 ? char.GetNumericValue(c).ToString() : c.ToString()));
+        }
+    }
+
+}
